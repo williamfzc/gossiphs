96a2195b462a8e50a5f542b7126b9426222fb0a9
williamfzc <178894043@qq.com>
2024-04-17 19:51:02 +0800

    chore(cli): option for saving json to file
    for enabling log at the same time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use gossiphs::graph::{Graph, GraphConfig};
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -21,10 +22,15 @@ enum SubCommand {
 #[derive(Parser, Debug)]
 struct RelateCommand {
     #[clap(long)]
+    #[clap(default_value = ".")]
     project_path: String,
 
     #[clap(long)]
     file: String,
+
+    #[clap(long)]
+    #[clap(default_value = None)]
+    json: Option<String>,
 }
 
 fn main() {
@@ -36,13 +42,21 @@ fn main() {
 }
 
 fn handle_relate(relate_cmd: RelateCommand) {
+    // result will be saved to file, so enable log
+    if !relate_cmd.json.is_none() {
+        tracing_subscriber::fmt::init();
+    }
     let mut config = GraphConfig::default();
     config.project_path = relate_cmd.project_path;
     let g = Graph::from(config);
     let files = g.related_files(&relate_cmd.file);
-    // convert to JSON and print to stdout
-    let json = serde_json::to_string_pretty(&files).unwrap();
-    println!("{}", json);
+
+    let json = serde_json::to_string(&files).unwrap();
+    if !relate_cmd.json.is_none() {
+        fs::write(relate_cmd.json.unwrap(), json).expect("");
+    } else {
+        println!("{}", json);
+    }
 }
 
 #[test]
@@ -50,6 +64,7 @@ fn test_handle_relate() {
     let relate_cmd = RelateCommand {
         project_path: String::from("."),
         file: "src/extractor.rs".to_string(),
+        json: None,
     };
     handle_relate(relate_cmd);
 }
