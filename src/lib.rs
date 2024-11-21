9037e38ca285c3f07dc09ef1eb3853b725415600
williamfzc <178894043@qq.com>
2024-11-22 00:11:57 +0800

    chore: expose more py class

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,25 @@ pub mod server;
 pub mod symbol;
 
 // py wrapper
-use crate::graph::{Graph, GraphConfig};
+use crate::graph::{Graph, GraphConfig, RelatedSymbol};
 use pyo3::prelude::*;
 
 mod pyapi;
 
+use crate::symbol::{DefRefPair, Symbol};
 use pyo3_stub_gen::define_stub_info_gatherer;
+use crate::api::{FileMetadata, RelatedFileContext};
 
 #[pymodule]
 fn _rust_api(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(pyapi::create_graph, m)?)?;
     m.add_class::<GraphConfig>()?;
     m.add_class::<Graph>()?;
+    m.add_class::<RelatedSymbol>()?;
+    m.add_class::<DefRefPair>()?;
+    m.add_class::<RelatedFileContext>()?;
+    m.add_class::<FileMetadata>()?;
+    m.add_class::<Symbol>()?;
     Ok(())
 }
 
